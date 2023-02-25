672d838a1301655a1550644ea1a07e50e3167d14
2020cs070@stu.ucsc.cmb.ac.lk <harirharilakshan@gmail.com>
2023-02-25 22:57:05 +0530

    new changes upload 25/02/2023

--- app/View/Sys_manager/subscription_sales.php
+++ app/View/Sys_manager/subscription_sales.php
@@ -2,7 +2,7 @@
 <?php require approot . '/View/inc/ManagerNavbar.php'; ?>
 <?php require approot . '/View/inc/manager_sidebar.php'; ?>
 
-    <div class="main-container">
+    <div class="main-container max-h-85">
     <div class="d-flex flex-column justify-content-center align-items-center ">
         <div class="subscription">
             <h2 class="title text-center">Subscription Sales</h2>
@@ -17,11 +17,11 @@
 <div class="d-flex flex-column">
     <div class="d-flex justify-content-evenly ">
         <div class="justify-content-center border border-primary my-3 mr-1 min-w-50">
-        <input type="text" id="searchPlan" style="height:100%;" onkeyup="myFunction()" placeholder="Search Plan......" class="w-100 h-100">
+        <input type="text" id="searchInput" value="" onchange="myFunction(this.value)" placeholder="Search Plan......" class="w-100 h-100">
         </div>
         <div class="my-3 d-flex mr-5">
         <a href="#">
-            <button class="btn btn-primary ml-2">SEARCH</button></a>
+            <button class="btn btn-primary ml-2" >SEARCH</button></a>
             <div class="d-flex text-center align-items-center text-md ml-2">FILTER
             </div>
         </div>
@@ -38,10 +38,10 @@
     <!-- <img src="<?php echo urlroot; ?>/public/img/sort.png" /> -->
 <!-- </div> -->
 
-
-<table class="table border border-primary w-95">
-    <tbody>
+<div class="w-100 h-50" style="overflow:auto; height:50vh;">
+<table id= "myTable" class="table border border-primary w-100 h-50">
             <tr class="min-w-75">
+            <thead>
             <tr>
                 <th>Plan Name</th>
                 <th>Price ($)</th>
@@ -49,78 +49,95 @@
                 <th>Plan Income ($)</th>
                 <th>Discount Details</th>
                 </tr>
-                <td class="td-1 w-25">Annual Plan</td>
+            </thead>
+
+    <tbody>
+
+        <?php
+
+            $arrlength = count($data);
+
+            for($x = 0; $x < $arrlength; $x++) {
+            echo '<tr>
+                <td class="td-1 w-25">'.$data[$x]->planName.'</td>
                 <div class="d-flex flex-column">
-                <td class="td-2">69</td>  
-                <td class="td-3">40</td>
-                <td class="td-4">2760</td>
+                <td class="td-2">'.$data[$x]->Price.'</td>  
+                <td class="td-3">'.$data[$x]->userCount.'</td>
+                <td class="td-4">'.$data[$x]->Price * $data[$x]->userCount.'</td>
                 </div>
                 <td class="td-5"><div class="d-flex flex-column">
                     <div class="d-flex flex-row text-black">
-                         Discount: 30%
+                         Discount: '.$data[$x]->Discount.'%
                     </div>
                     <button class="btn btn-info my-1" onclick="openPopup()">EDIT</button>
                     <div class="dialog-box-outer" id="popup">
-                    <div class="popup mx-1 my-1 px-1 py-1 min-w-40 min-h-50">
-                    <div class="border border-primary w-10 d-flex justify-content-center align-items-center">
-                        <div class="text-black">Discount 
-                    <input class="w-10 bg-primary" type="number" id="discount">
+                    <div class="popup mx-1 my-1 px-1 py-1 min-w-20 min-h-25">
+                    <div class="my-4 w-100 d-flex flex-row justify-content-center align-items-center">
+                        <div class="text-black w-50">
+                        <label>Discount (%) :</label>
+                        </div>
+                    <div class="mr-2 text-black w-40 h-50 border border-primary">
+                    <input class="px-1 bg-primary w-50" type="number" id="discount">
+                    </div>
+                    </div>          
+                    <div class="d-flex flex-row mx-5 my-3" onclick="closePopup()">
+                    <div class="justify-content-start">
+                        <div class="btn btn-primary ">Save</div>
+                    </div>
+                    <div class="justify-content-end">
+                        <div class="btn btn-primary mx-5">cancel</div>
+                    </div></div>
                     </div>
                     </div>
-                    <div class="d-flex align-items-flex-end justify-content-end" onclick="closePopup()">
-                    <div class="d-flex flex-row justify-content-center align-items-center">
-                        <div class="btn btn-primary">Save</div>
-                        <div class="btn btn-primary">cancel</div>
                     </div>
-                    
-                    </div>
-                    </div>
-                    </div>
-                    </div>
-                    </div>
-                    </div>
+                </td>
+            </tr>';
+            }
+        ?>
 
-        </div></td>
-       </tr>
-
-       <tr class="min-w-25">
-        <td class="td-1 w-30">Starter Plan</td>
-        <div class="d-flex flex-column">
-        <td class="td-2">90</td>
-        <td class="td-3">45</td>
-        <td class="td-4">4050</td>
-            <!-- <div class="my-1 d-flex flex-column">
-                <button class="btn btn-dark" >Promote Site</button>
-            </div>
-            <div class="d-flex flex-column">
-                <button class="btn btn-dark">Email Promote</button>
-            </div> -->
-        </div>
-        <td class="td-4"><div class="d-flex flex-column">
-            <div class="d-flex flex-row text-black">
-                Discount: 10%
-            </div>
-            <button class="btn btn-info my-1">EDIT</button>
-        </div></td>
-       </tr>
+       <!-- <tr class="min-w-75">
+       <td class="td-1 w-25">Starter Plan</td>
+                <div class="d-flex flex-column">
+                <td class="td-2">49</td>  
+                <td class="td-3">40</td>
+                <td class="td-4">1960</td>
+                </div>
+                <td class="td-5"><div class="d-flex flex-column">
+                    <div class="d-flex flex-row text-black">
+                         Discount: 20%
+                    </div>
+                    <button class="btn btn-info my-1" onclick="openPopup()">EDIT</button>
+                    <div class="dialog-box-outer" id="popup">
+                    <div class="popup mx-1 my-1 px-1 py-1 min-w-20 min-h-25">
+                    <div class="my-4 w-100 d-flex flex-row justify-content-center align-items-center">
+                        <div class="text-black w-50">
+                        <label>Discount (%) :</label>
+                        </div>
+                    <div class="mr-3 text-black w-40 h-50 border border-primary">
+                    <input class="px-1 bg-primary w-50" type="number" id="discount">
+                    </div>
+                    </div>          
+                    <div class="d-flex flex-row mx-5 my-3" onclick="closePopup()">
+                    <div class="justify-content-center align-items-center">
+                        <div class="btn btn-primary ">Save</div>
+                    </div>
+                    <div class="justify-content-center align-items-center">
+                        <div class="btn btn-primary mx-5" style="margin-left: 150px;">cancel</div>
+                    </div></div>
+                    </div>
+                    </div>
+                    </div>
+                    </td>
+                </tr> -->
     </tbody>
 </table>
+</div>
+</div>
+</div>
 
-<!-- <div class="d-flex flex-row mb-2">
-    <label> : </label>
-    
-        
-        
-    </div>
-         -->
-</div>
-</div>
-</div>
-<script src="../../public/js/main.js"></script>
+<script src="/ezvote/public/js/main.js"></script>
+<script src="/ezvote/public/js/search.js"></script>
 <?php require approot . '/View/inc/footer.php'; ?>
 
 
 
-<!-- </body>
-
-</html> -->
