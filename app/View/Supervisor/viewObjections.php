99bca5ff9d4c3081b9932005697af0de7812eb57
Divaka Perera <2020cs129@stu.ucsc.cmb.ac.lk>
2023-04-26 19:36:09 +0530

    autosave

--- app/View/Supervisor/viewObjections.php
+++ app/View/Supervisor/viewObjections.php
@@ -11,61 +11,65 @@
     </div>
     <div id="objectionArea" class="d-flex flex-wrap w-100 bg-secondary overflow-scroll justify-content-center p-1 mt-1 mb-1">
         <?php
-        if (empty($data['objections'])) {
-            echo "<h1 class='text-center text-danger mt-5'>No Objections</h1>";
-        }
-        foreach ($data['objections'] as $objectionRow) {
+
+        foreach ($data['candidates'] as $candidate) {
+            $count = 0;
+            foreach ($data['objections'] as $objection) {
+                if ($candidate->candidateId == $objection->CandidateID) {
+                    $count++;
+                }
+            }
+            if ($count > 0) {
         ?>
-            <div class="popup-window-1 bg-secondary min-h-30 min-w-30 text-center border-1 border-radius-2" id="popup-<?php echo $objectionRow->ObjectionID; ?>" style="display: none;">
+        <div class="card d-flex flex-column">
+            <div class="sub-title mb-auto mt-1">
+                <?php 
+                    echo $candidate->candidateName;
+                ?>
+            </div>
+            <div class="text-danger my-1 text-xl">
+                <?php echo $count; ?> objection(s)
+            </div>
+            <div>
+            <button id="<?php echo $candidate->candidateId; ?>" class="border-none border-radius-5 text-2xl w-45 h-75 mt-auto mb-1 mx-auto bg-primary text-white" style="cursor: pointer;" onclick="objectionPopUp(this.id)"><i class="fa-solid fa-eye"></i></button>
+            </div>
+        </div>
+        <?php
+            }
+        ?>
+            <div class="popup-window-1 bg-secondary min-h-30 min-w-45 text-center border-1 border-radius-2" id="popup-<?php echo $candidate->candidateId; ?>" style="display: none;">
                 <div class="popup-window-1-content bg-light border-radius-2 p-2">
-                    <div class="title"><?php echo $objectionRow->Subject; ?></div>
-                    <div class="text-x"><?php echo $objectionRow->Description; ?></div>
-                    <div class="text-xl my-1">
-                        Objection By: <b>
-                            <?php
-                            foreach ($data['voters'] as $voter) {
-                                if ($voter->voterId == $objectionRow->VoterID) {
-                                    echo $voter->Name;
-                                }
+                    <?php 
+                        foreach($data['objections'] as $objection){
+                            if($candidate->candidateId == $objection->CandidateID){
+                    ?>
+                        <div class="w-100 d-flex flex-column my-1 border-radius-2">
+                            <div class="w-100 bg-primary border-radius-2 px-1 d-flex text-white">
+                                <div class="text-x mr-auto my-auto">Subject...</div>
+                                <div class="ml-auto my-auto"><button id="<?php echo $objection->ObjectionID ?>" class="btn bg-primary text-white text-x" onclick="expandDiv(this.id)"><i class="fa-solid fa-angle-right"></i></button></div>
+                            </div>
+                            <div class="w-100 border-radius-2 my-1" style="display: none;" id="expand-<?php echo $objection->ObjectionID; ?>" >
+                                
+                            </div>
+                        </div>
+                    <?php 
                             }
-                            ?>
-                        </b>
-                    </div>
-                    <div class="text-xl my-1">
-                        Against: <b>
-                            <?php
-                            foreach ($data['candidates'] as $candidateRow) {
-                                if ($candidateRow->candidateId == $objectionRow->CandidateID) {
-                                    echo "<a href='".urlroot."/Pages/viewCandidate/$candidateRow->candidateId' >$candidateRow->candidateName</a>";
-                                }
-                            }
-                            ?>
-                        </b>
-                    </div>
+                        }
+
+                    ?>
                     <div>
                         <button class="btn btn-danger my-1"><b>Remove Candidate</b></button>
                         <button class="btn btn-danger my-1"><b>Remove Objection</b></button>
                     </div>
                     <div>
-                        <button class="btn btn-primary my-1" id="<?php echo $objectionRow->ObjectionID; ?>" onclick="objectionPopUp(this.id)"><b>Cancel</b></button>
+                        <button class="btn btn-primary my-1" id="<?php echo $candidate->candidateId; ?>" onclick="objectionPopUp(this.id)"><b>Cancel</b></button>
                     </div>
                 </div>
-            </div>
-            <div class="card">
-                <?php
-                foreach ($data['candidates'] as $candidateRow) {
-                    if ($candidateRow->candidateId == $objectionRow->CandidateID) {
-                        echo "<div class='text-xl mt-auto text-primary'><b>" . $candidateRow->candidateName . "</b></div>";
-                    }
-                }
-                ?>
-                <button id="<?php echo $objectionRow->ObjectionID; ?>" class="border-none border-radius-5 text-2xl w-45 h-25 mt-auto mb-1 mx-auto bg-primary text-white" style="cursor: pointer;" onclick="objectionPopUp(this.id)"><i class="fa-solid fa-eye"></i></button>
             </div>
         <?php
         }
         ?>
     </div>
-
 </div>
 
 <script>
@@ -78,6 +82,18 @@
         } else {
             popup.style.display = "none";
             document.querySelector('body').classList.remove('no-scroll-for-popup');
+        }
+    }
+    function expandDiv(id){
+        var elementDiv = document.getElementById("expand-" + id);
+        var elementButton = document.getElementById(id);
+        if(elementDiv.style.display === "none"){
+            elementDiv.style.display = "block";
+            elementButton.innerHTML = '<i class="fa-solid fa-angle-down"></i>';
+            var others = elementButton.parentNode.parent
+        }else{
+            elementDiv.style.display = "none";
+            elementButton.innerHTML = '<i class="fa-solid fa-angle-right"></i>';
         }
     }
 </script>
